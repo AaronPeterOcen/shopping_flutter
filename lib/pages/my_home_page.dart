0596b8065d2c128ce9e0b93b0e44bb92ca99faca
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2025-02-12 23:07:50 +0300

    cleaning up the homepage

---
+++ lib/pages/my_home_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  List<Product> _products = [
+    Product('Product 1', 10.99),
+    Product('Product 2', 5.99),
+    Product('Product 3', 7.99),
+  ];
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ecommerce App'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(
+            'You have pushed the button this many times:',
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _products.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_products[index].name),
+                  subtitle: Text('Price: ${_products[index].price}'),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class Product {
+  String name;
+  double price;
+
+  Product(this.name, this.price);
+}
